069be7ae4bccf9bc3f6208dfdd602e11fced957f
ihor.zadorozhnyi <izdev.digital@gmail.com>
2025-11-29 11:15:37 +0100

    access hass environment as root, but execute the app as user

--- e-paper-dashboard/run.sh
+++ e-paper-dashboard/run.sh
@@ -8,5 +8,9 @@ export SUPERUSER_PASSWORD=$(bashio::config 'superuser_password')
 bashio::log.info "Starting E-Paper Dashboard..."
 bashio::log.info "Client URL: ${CLIENT_URL}"
 
-# Start the application
-exec dotnet /app/EPaperDashboard.dll
+# Start the application as app user with environment variables
+exec su-exec app env \
+    CLIENT_URL="${CLIENT_URL}" \
+    SUPERUSER_USERNAME="${SUPERUSER_USERNAME}" \
+    SUPERUSER_PASSWORD="${SUPERUSER_PASSWORD}" \
+    dotnet /app/EPaperDashboard.dll
